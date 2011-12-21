1a77513c2df6ebdfa0b17c5ccbba6048a9413cc3
Bradley Wright <brad@intranation.com>
2011-12-21 22:07:17 +0000

    First pass at a Hogan compiler that prints compiled templates
    - Arguments are all filenames
    - Templates are sent to `stdout` only
    - Templates aren't named in output, so no way to retrieve yet (TODO)

---
+++ bin/hogan-compile.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/*
+ * Compiles Hogan templates and prints result to stdout
+ */
+
+var hogan = require('../lib/hogan'),
+    path = require('path'),
+    fs = require('fs');
+
+// we assume that argv[2] is a list of files, or a directory
+// that contains mustache templates
+var argv = process.argv.slice(2);
+
+argv.map(function(filePath) {
+    var fullPath = path.resolve(__dirname, filePath),
+        openedFile = fs.readFileSync(fullPath, 'utf-8');
+    if (openedFile) {
+        process.stdout.write(hogan.compile(openedFile, {asString:true}));
+    }
+});
+
+process.exit(0);
