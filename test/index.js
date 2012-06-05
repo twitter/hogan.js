20dfa99fdaece1b014939e0ad45a3b482761e97a
Matt Cheely <matt.cheely@gmail.com>
2012-06-05 19:41:17 -0400

    Fix lambdas getting wrong arguments in inheritance subsections

--- test/index.js
+++ test/index.js
@@ -1100,3 +1100,18 @@ test("Section With Custom Uneven Delimiter Length", function() {
   var s = t.render(context);
   is(s, 'Test<b>bar</b>', 'Section content is correct with uneven reset delimiter length');
 });
+
+
+test("Lambda expression in inherited template subsections", function() {
+    var lambda = function() {
+        return function(argument) {
+            return 'altered ' + argument;
+        }
+    }
+    var partial = '{{$section1}}{{#lambda}}parent1{{/lambda}}{{/section1}} - {{$section2}}{{#lambda}}parent2{{/lambda}}{{/section2}}';
+    var text = '{{< partial}}{{$section1}}{{#lambda}}child1{{/lambda}}{{/section1}}{{/ partial}}'
+    var template = Hogan.compile(text);
+
+    var result = template.render({lambda: lambda}, {partial: Hogan.compile(partial)});
+    is(result, 'altered child1 - altered parent2', 'Lambda replacement failed with template inheritance');
+});
